f343e836da2611ec0def576ae9545cf2c10c69ba
thankgod <ugothankgod729@gmail.com>
2023-11-19 12:15:53 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import inputRow from './inputRow';
+
+const BASE_URL = 'https://api.frankfurter.app/latest'
 
 function App() {
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(date => console.log(data))
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1> Convert Currency</h1>
+    <inputRow />
+    <div className='equals'> = </div>
+    <inputRow />
+    </>
+
   );
 }
 
---
+++ src/inputRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function inputRow() {
+  return (
+    <div>
+      <input className='input' type="number"/>
+      <select>
+        <option value="hi"> hi</option>
+      </select>
+    </div>
+  )
+}
